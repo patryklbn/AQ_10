14cfbdb064057a450cb005fe31254a6d6248ec56
patrykgronczewski <40599647@live.napier.ac.uk>
2024-02-12 14:23:03 +0000

    Initial commit of project including tests

---
+++ AQ_10/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AQ_10;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ AQ_10/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AQ_10;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
