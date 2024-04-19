6c924d8d1b48b2b9b0eb13d5bd629ec5a0f1a3c8
patrykgronczewski <40599647@live.napier.ac.uk>
2024-04-19 14:30:57 +0100

    Add collection for answerservice and endpage viewmodel

---
+++ Tests/AnswersServiceTestCollection.cs
@@ -0,0 +1,7 @@
+﻿using Xunit;
+
+[CollectionDefinition("AnswersService Tests", DisableParallelization = true)]
+public class AnswersServiceTestCollection
+{
+    // This class doesn't contain any code. It's just used for the collection definition.
+}
